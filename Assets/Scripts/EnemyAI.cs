0aa2775e59e1145058db4e044fec5b8fa3506034
Egor1955 <133893560+Egor1955@users.noreply.github.com>
2024-02-20 17:18:26 +0300

    3

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,18 +5,81 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform targetPoint;
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
 
     private NavMeshAgent _navMeshAgent;
+
+    private bool _isPlayerNoticed;
+
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickNewPatrolPoint();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        _navMeshAgent.destination = targetPoint.position;
+        var direction = player.transform.position - transform.position;
+
+        if (Vector3.Angle(transform.forward, direction)< viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+                else
+                {
+                    _isPlayerNoticed = false;
+                }
+            }
+            else
+            {
+                _isPlayerNoticed = false;
+            }
+        }
+        else
+        {
+            _isPlayerNoticed = false;
+        }
+
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+
+        ChaseUpdate();
+        PatrolUpdate();
+    }
+    private void PatrolUpdate()
+    {
+        if (!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
     }
 }
